3d5f3958ac9a24d73646c0312967f24d871108c6
nguyenhuuchiitech <nguyenhuuchidlu@gmail.com>
2020-02-28 07:47:07 +0700

    Fix issues in Issue List Excel

---
+++ NextTech.ChaChing123.UI.Web/Scripts/spa_portal/assets/js/controllers/leadAddEditCtrl.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+/** 
+  * controller for User Profile Example
+*/
+
+var backgroundPath = "";
+var resourcePath = "";
+var baseUrl = 'https://api.123chaching.app';
+//var baseUrl = 'http://localhost:1494';
+
+app.controller('LeadAddEditCtrl', ["$scope", "$window", "$location", "$localStorage", "$timeout", "membershipService", "editorService", "funnelsService", "notificationService",
+    function ($scope, $window, $location, $localStorage, $timeout, membershipService, editorService, funnelsService, notificationService) {
+        var username = ($localStorage.currentUser) ? $localStorage.currentUser.username : "";
+        var sessionKey = ($localStorage.currentUser) ? $localStorage.currentUser.token : "";
+
+        // JSON ARRAY TO POPULATE TABLE.
+
+        function loadLeads() {
+            $scope.showSpinner = true;
+        }
+
+        $scope.soloPageManager = {
+            init: function () {
+                loadLeads();
+            }
+        };
+
+        $scope.soloPageManager.init();
+    }]);
